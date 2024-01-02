a4f1dcb996fa29671c243e40e0cda3d95217cc2d
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2024-01-02 21:03:16 +0000

    Authorization3

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,15 +1,7 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-// import { ContactForm } from 'components/ContactForm/ContactForm';
-import {
-  selectContacts,
-  selectError,
-  selectLoading,
-} from '../redux/contacts/contactsSelectors';
-// import { ContactList } from 'components/ContactList/ContactList';
-// import { FilterContact } from 'components/Filter/Filter';
+import { useDispatch } from 'react-redux';
+
 import * as contactsOperation from '../redux/contacts/contactsOperation';
-// import { Loader } from 'components/Loader/Loader';
 import { UserContacts } from 'components/UserContacts/UserContacts';
 
 const Contacts = () => {
